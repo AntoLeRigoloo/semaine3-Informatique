9575b925e06051cb3a96bf4026533ee05ceacff5
AntoLeRigoloo <92756820+AntoLeRigoloo@users.noreply.github.com>
2023-01-19 14:09:41 +0100

    version jeudi midi

--- DjangoApp/Main/static/script.js
+++ DjangoApp/Main/static/script.js
@@ -1,55 +1,112 @@
-
 let btn = document.getElementById("btn");
+
+let boxJSON = document.getElementById("boxJSON");
 let JSON = document.getElementById("JSON");
-let JS = document.getElementById("JS");
-let CSS = document.getElementById("CSS");
 
-let obj;
-
-function onChange(event) {
+function onDragAndDropJSON(event){
+    event.preventDefault();
+    let fileType = event.dataTransfer.files[0].type;
+    if (fileType != "application/json") {
+        alert("Please drop a json file");
+        boxJSON.style.backgroundColor = "white";
+        return;
+    }
+    else{
+        file = event.dataTransfer.items[0].getAsFile();
+        let fileReader = new FileReader();
+        fileReader.onload = onReaderLoadJSON;
+        fileReader.readAsText(file);
+    }
+}
+function onChangeJSON(event) {
     var reader = new FileReader();
     console.log("json triggers")
-    reader.onload = onReaderLoad;
+    reader.onload = onReaderLoadJSON;
     reader.readAsText(event.target.files[0]);
 }
-function onReaderLoad(event){
+function onReaderLoadJSON(event){
     console.log(event.target.result);
     JSON.value = event.target.result;
 }
-document.getElementById('JSON-input').addEventListener('change', onChange);
+document.getElementById('JSON-input').addEventListener('change', onChangeJSON);
+boxJSON.addEventListener("drop", onDragAndDropJSON);
+boxJSON.addEventListener("dragover", (event)=>{event.preventDefault(); boxJSON.style.backgroundColor = "red";});
+boxJSON.addEventListener("dragleave", ()=>{boxJSON.style.backgroundColor = "white";});
+  
 
 
+let JS = document.getElementById("JS");
+let boxJS = document.getElementById("boxJS");
 
-
-function onChangeJavascript(event) {
+function onDragAndDropJS(event){
+    event.preventDefault();
+    let fileType = event.dataTransfer.files[0].type;
+    if (fileType != "text/javascript") {
+        alert("Please drop a javascript file");
+        boxJS.style.backgroundColor = "white";
+        return;
+    }
+    else{
+        file = event.dataTransfer.items[0].getAsFile();
+        let fileReader = new FileReader();
+        fileReader.onload = onReaderLoadJS;
+        fileReader.readAsText(file);
+    }
+}
+function onChangeJS(event) {
     var reader = new FileReader();
-    console.log("javascript triggers")
-    reader.onload = onReaderLoadJavascript;
+    console.log("js triggers")
+    reader.onload = onReaderLoadJS;
     reader.readAsText(event.target.files[0]);
 }
-function onReaderLoadJavascript(event){
+function onReaderLoadJS(event){
     console.log(event.target.result);
     JS.value = event.target.result;
-
 }
-document.getElementById('JS-input').addEventListener('change', onChangeJavascript);
-
-
-
-function onChangeCSS(event) {
-    var reader = new FileReader();
-    console.log("CSS triggers")
-    reader.onload = onReaderLoadJavascript;
-    reader.readAsText(event.target.files[0]);
-}
-function onReaderLoadCSS(event){
-    console.log(event.target.result);
-    CSS.value = event.target.result;
-
-}
-document.getElementById('CSS-input').addEventListener('change', onChangeCSS);
+document.getElementById('JS-input').addEventListener('change', onChangeJS);
+boxJS.addEventListener("drop", onDragAndDropJS);
+boxJS.addEventListener("dragover", (event)=>{event.preventDefault(); boxJS.style.backgroundColor = "red";});
+boxJS.addEventListener("dragleave", ()=>{boxJS.style.backgroundColor = "white";});
+  
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
--- DjangoApp/Main/views.py
+++ DjangoApp/Main/views.py
@@ -1,11 +1,9 @@
 from django.shortcuts import render
 import json
 import openai
-from .forms import QuestionForm
-from .models import Question
 import os
 
-openai.api_key = "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"
+openai.api_key = "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"
 
 def CallChatGpt(question,reponse):
     questionAposer = "Partons du principe que tu es un professeur ayant realise un examen. a la question comportant la consigne : {}. ton eleve a repondu : {}. est-ce que cette réponse est correcte ou incorrecte ? Réponds uniquement en un seul mot sans utilisation de points à la fin de ta phrase".format(question,reponse)
@@ -19,82 +17,34 @@ def CallChatGpt(question,reponse):
     res = res["choices"][0]["text"]
     return res
 
-    
-
-
-
-
-
-# def index(request):
-#     if request.method == "POST": #check if request is POST
-#         form = QuestionForm(request.POST, request.FILES) #get the form from the request using the model
-#         if form.is_valid():
-#             if os.listdir("media/question"): #check if there is a file in the folder
-#                 for file in os.listdir("media/question"): 
-#                     os.remove("media/question/"+file) #remove all files in the folder
-#             form.save() #save the new file in the folder
-#             os.rename("media/question/"+os.listdir("media/question")[0],"media/question/data.json") #rename the file to data.json
-#     else :
-#         form = QuestionForm()
-
-#     return render(request, 'main/index.html', {'form': form})
-def index(request):
-    if request.method == "POST": #check if request is POST
-        form = QuestionForm(request.POST, request.FILES) #get the form from the request using the model
-        if form.is_valid():
-            file = request.FILES.dict()['ListeQuestion']
-            #give me the content of file in json format
-            jsonfile = json.loads(file.read().decode('utf-8'))
-            json_object = json.dumps(jsonfile, ensure_ascii=False, indent=4)
-            with open("media/question/data.json", "w") as outfile:
-                outfile.truncate(0)
-                outfile.write(json_object)
-            
-            
-    else :
-        form = QuestionForm()
-
-    return render(request, 'main/index.html', {'form': form})
-
-
-
-
 
 
 def Configuration(request):
     if request.method == "POST":
-        with open("main/static/functiontest.js", "w") as outfile:
-            outfile.truncate(0)
-            outfile.write(request.POST.dict()["JS"])
-        
-        file = json.loads(request.POST.dict()["JSON"])
-        json_object = json.dumps(file, ensure_ascii=False, indent=4)
-        with open("media/question/data.json", "w") as outfile:
-            outfile.truncate(0)
-            outfile.write(json_object)
-
-        with open("main/static/styleQuestionnaire.css", "w") as outfile:
-            outfile.truncate(0)
-            outfile.write(request.POST.dict()["CSS"])
-
+        print (request.POST.dict())
+        try :
+            if request.post.dict()["JS"] != "":
+                with open("main/static/functiontest.js", "w") as outfile:
+                    outfile.truncate(0)
+                    outfile.write(request.POST.dict()["JS"])
+        except:
+            pass
         
 
-        
-
-        
-
-
+        try :
+            file = json.loads(request.POST.dict()["JSON"])
+            json_object = json.dumps(file, ensure_ascii=False, indent=4)
+            with open("media/question/data.json", "w") as outfile:
+                outfile.truncate(0)
+                outfile.write(json_object)
+        except:
+            pass
     return render(request, 'main/configuration.html')
 
 
 def Questionnaire(request):
     with open("media/question/data.json") as json_file:
-        jsonfile = json.dumps(json.load(json_file))
-
-    if request.method == "POST":
-        print(request.POST)
-        
-    
+        jsonfile = json.dumps(json.load(json_file))    
     context = {
         "jsonfile" : jsonfile,
     }
@@ -102,7 +52,54 @@ def Questionnaire(request):
     return render(request, 'main/questionnaire.html',context)
 
 
+def Note(liste_reponse):
+    '''prend en argument une liste de [".Correcte",".Incorrecte"] de la longueure du nombre de question
+    retourne un liste avec [nombre de point, total points du questionnaire, note sur 20]'''
 
+    with open("media/question/data.json") as json_file:
+        data = json.load(json_file)
+
+    # access the QCM section
+    qcm_data = data['QCM']
+    total_points = []
+    bareme = 0
+    reponse = 0
+    for question in qcm_data.values() :
+        bareme += int(question['Points'])
+        if (liste_reponse[reponse]=='.\n\nCorrecte'):
+            total_points.append(int(question['Points']))
+        reponse += 1
+
+    return [sum(total_points),bareme,sum(total_points)*20/bareme]
+
+def Correction(request):
+    with open("media/question/data.json") as json_file:
+        jsonfile = json.load(json_file)
+
+    nombre_question=len(jsonfile["QCM"])
+    question=[]
+    reponse_list=[]
+
+    for o in range (nombre_question):
+        question.append(jsonfile["QCM"]["{}".format(o+1)]["Question"])
+
+    if request.method == "POST": 
+        Correction=[]
+        reponsePost = request.POST.dict()
+
+        for i in range (nombre_question):
+            Correction.append(CallChatGpt(question[i],reponsePost["Question{}".format(i+1)]))
+
+        for i in range (nombre_question):
+            reponse_list.append(reponsePost["Question{}".format(i+1)])
+
+        note=Note(Correction)[2]
+        ElementsQuestion=zip(question,Correction,reponse_list)
+        context = {'ElementsQuestion':ElementsQuestion}
+        context['note'] = note
+        print(Correction)
+
+    return render(request,'main/correction.html',context)
 
 
 
